3d9bba97107f9415b7ce7a25608febd67c37f828
Sten Nick <stennick01@gmail.com>
2019-02-05 16:07:44 +0300

    created more fixtures

---
+++ fixture/openSession.py
@@ -0,0 +1,15 @@
+"""
+тут нету вызова webdriver'a
+для доступа к драйверу, мы обращаемся к модулю Application
+данный модуль мы вызываем в __init__ модуля Application
+после чего получаем доступ к драйверу
+"""
+class Page:
+
+    def __init__(self, app): # этот модуль вызвали в Application, и ссылаемся на него для вызова драйвера
+        self.app = app
+
+    def openPage(self):
+        wd = self.app.wd # обращаемся к модулую Application
+        wd.get("https://www.tutu.ru/")
+
--- test/test_init_tests.py
+++ test/test_init_tests.py
@@ -8,19 +8,17 @@ from fixture.application import Application
 @pytest.fixture # тут мы сказали что создаем фикстуру
 def app(request): # 'request будет принимать свойство закрытия браузера
     fixture = Application() # ссылаемся на модуль main_fixture с классом Application
-    request.addfinalizer(fixture.closeBrowser) # тут вызываем закрытие браузера
+    request.addfinalizer(fixture.closeSession.closeBrowser) # тут вызываем закрытие браузера
     return fixture
     # addfinalizer это свойство фикстуры из либы pytest'a
 
 
 
 def test_group(app): # app это фикстура со всем нужными тестами
-    app.openPage()
-    app.find_teckets(city_from="Мсоква", city_to="Париж")
-    app.check_date(date_from="25.04.2019", date_back="04.05.2019")
-    app.search()
+    app.openSession.openPage(url_page="https://www.tutu.ru/")
+    app.findTickets.find_teckets(city_from="Мсоква", city_to="Париж")
+    app.getDate.check_date(date_from="25.04.2019", date_back="04.05.2019")
+    app.search.search()
+    app.closeSession.closeBrowser()
 
-
-def tearDown(app): # вызов идет через фикстуру app ^^
-    app.closeBrowser()
 
